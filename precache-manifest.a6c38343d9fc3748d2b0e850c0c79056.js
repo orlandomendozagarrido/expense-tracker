5f1f9420e72328be78061eac7ce54fbe8b62d1e9
orlandomendozagarrido <orlandomendozagarrido206@gmail.com>
2020-03-13 15:31:04 -0600

    Updates

---
+++ precache-manifest.a6c38343d9fc3748d2b0e850c0c79056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e19a2e4aa737e094f151a97cace38c9",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "30d822aa81a7f497fb75",
+    "url": "/expense-tracker/static/css/main.6a3ecdbd.chunk.css"
+  },
+  {
+    "revision": "549367768d806fd7cd59",
+    "url": "/expense-tracker/static/js/2.37aa6943.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/expense-tracker/static/js/2.37aa6943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d822aa81a7f497fb75",
+    "url": "/expense-tracker/static/js/main.6bec00c6.chunk.js"
+  },
+  {
+    "revision": "6a41a662455943274e66",
+    "url": "/expense-tracker/static/js/runtime-main.a3b1366d.js"
+  }
+]);
